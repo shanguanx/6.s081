e6eef863ed6630c1f05c4210865ff8acebc4beaf
shanguanx <37698009+shanguanx@users.noreply.github.com>
2022-06-05 17:44:57 +0800

    Update primes.c

--- lab1/primes.c
+++ lab1/primes.c
@@ -10,42 +10,35 @@ main(int argc, char *argv[]) {
         exit(1);
     }
 
-    int p[40][2];
+    int p[100][2];
     int pos = 0;
     pipe(p[pos]);
 
-    for (int i = 2; i <= 35; i++) {
+    for (int i = 2; i <= 100; i++) {
         write(p[pos][1], &i, 4);
     }
     close(p[pos][1]);
 
+
     while (1) {
-        int a[40];
+        int a[100];
         //从左侧读入
         int n;
-        for (n = 0;; n++) {
-            read(p[pos][0], &a[n], 4);
-            if (a[n] == 0)break;
-        }
-        //child close read
-        close(p[pos][0]);
-
-        printf("prime %d\n", a[0]);
-        if (a[0] == 31)break;
         pipe(p[++pos]);
+        for (n = 0;; n++) {
+            read(p[pos-1][0], &a[n], 4);
+            if (a[n] == 0)break;
+            if (a[n] % a[0] == 0)continue;
+            write(p[pos][1], &a[n], 4);
+        }
+        //close read
+        close(p[pos-1][0]);
+        //close Write
+        close(p[pos][1]);
+        printf("prime %d\n", a[0]);
+        if (a[0] == 97)break;
         if (fork() == 0) {
-            //向右侧写入 //parnt close read
-            close(p[pos][0]);
-            for (int i = 1; i < n; ++i) {
-                if (a[i] % a[0] == 0)continue;
-                write(p[pos][1], &a[i], 4);
-            }
-            //parnt close write
-            close(p[pos][1]);
             exit(0);
-        } else {
-            //child close write
-            close(p[pos][1]);
         }
     }
     exit(0);
